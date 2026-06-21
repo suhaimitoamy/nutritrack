0d0a6b89c8e3ece002307edea88c4be8c50ed332
suhaimitoamy <suhaimitoamy@gmail.com>
2026-06-21 16:43:51 +0800

    fix apk build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,12 +7,12 @@ plugins {
 
 android {
   namespace = "com.example"
-  compileSdk = 36
+  compileSdk = 35
 
   defaultConfig {
     applicationId = "com.aistudio.kaloriku.xwpyzg"
     minSdk = 24
-    targetSdk = 36
+    targetSdk = 35
     versionCode = 1
     versionName = "1.0"
 
